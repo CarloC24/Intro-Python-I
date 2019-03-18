e926d7658e027f009d863201101056ab541636b6
CarloC24 <carlo.clamucha@gmail.com>
2019-03-18 17:08:29 -0500

    last 1 challenge

--- src/classes.py
+++ src/classes.py
@@ -2,7 +2,12 @@
 # constructor
 
 # YOUR CODE HERE
-        
+
+class latLng:
+    asdwa
+
+
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
